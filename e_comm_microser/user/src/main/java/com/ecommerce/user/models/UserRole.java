512cea7afbe5a92f00fd8bce3797da8be354af89
ramcruise <rambabu0216@gmail.com>
2026-03-11 23:26:21 +0530

    remove redundant service

--- e_comm_microser/user/src/main/java/com/ecommerce/user/models/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.ecommerce.user.models;
-
-public enum UserRole {
-    CUSTOMER, ADMIN
-}
--- e_comm_microser/user/src/main/java/com/ecommerce/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.user.repository;
-
-import com.ecommerce.user.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository{ //extends JpaRepository<User, String> {
-}
